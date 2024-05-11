f06fb7ea3fad663da06c4107d4a830229090a232
Nicodragon25 <nicolasariellaure@gmail.com>
2024-05-11 19:25:49 -0300

    Add show and hide methods to room

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -7,11 +7,29 @@ public class Room : MonoBehaviour
     [SerializeField] private List<Room> adjacentRooms = new List<Room>();
     [SerializeField] private List<Wall> walls = new List<Wall>();
 
+    public bool shouldBeDrawn = false;
+    public bool isChecked = false;
     private void Start()
     {
         foreach (Wall wall in transform.GetComponentsInChildren<Wall>())
         {
             walls.Add(wall);
+        }
+    }
+
+    public void Show()
+    {
+        foreach (MeshRenderer mesh in transform.GetComponentsInChildren<MeshRenderer>())
+        {
+            mesh.enabled = true;
+        }
+    }
+
+    public void Hide()
+    {
+        foreach (MeshRenderer mesh in transform.GetComponentsInChildren<MeshRenderer>())
+        {
+            mesh.enabled = false;
         }
     }
 }
