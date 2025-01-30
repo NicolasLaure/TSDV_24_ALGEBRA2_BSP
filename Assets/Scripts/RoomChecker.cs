310048fc2848ea640ca4447a8a224e1c39b15689
Nicodragon25 <nicolasariellaure@gmail.com>
2025-01-30 15:08:57 -0300

    Remove unnecessary packages and scripts

--- Assets/Scripts/RoomChecker.cs
+++ Assets/Scripts/RoomChecker.cs
@@ -1,8 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using CustomMath;
 using System.Linq;
+using CustomMath;
+using UnityEngine;
 
 public class RoomChecker : MonoBehaviour
 {
